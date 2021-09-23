db52d2ae59617c28cdfde90466ffefeddefc07e5
Debananda Nayak <rdebanandanayak@gmail.com>
2021-09-24 00:03:54 +0530

    Intial release

--- extension.js
+++ extension.js
@@ -1,62 +1,90 @@
-const vscode = require('vscode')
-const fs = require('fs')
-const ytdl = require('ytdl-core');
+const vscode = require("vscode")
+const fs = require("fs")
+const ytdl = require("ytdl-core")
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 
 function activate(context) {
+  console.log(
+    'Congratulations, your extension "youtube-downloader" is now active!'
+  )
+  let disposable = vscode.commands.registerCommand(
+    "youtube-downloader.YouTubeVideoDownload",
+    async function () {
+      const date = new Date()
+      const url = await vscode.window.showInputBox().then(data => data.trim())
+      // if (url == null) return
+      if (!ytdl.validateURL(url)) return
 
+      const infos = await await ytdl.getInfo(url)
+      // console.log(infos)
+      const title = infos.videoDetails.title
+      // console.log(infos.formats);
+      const videoQualityInfos = infos.formats
+        .filter(info => {
+          return info.hasAudio == true && info.hasVideo == true
+          // return info.mimeType.includes('video/mp4' ||)
+        })
+        .map(data => {
+          return {
+            label: `${title} - ${data.qualityLabel}`,
+            detail: `${(parseInt(data.contentLength) / (1024 * 1024)).toFixed(
+              2
+            )} MB`,
+            format: data.quality,
+            itag: data.itag.toString(),
+          }
+        })
 
-	console.log('Congratulations, your extension "youtube-downloader" is now active!')
-	let disposable = vscode.commands.registerCommand('youtube-downloader.YouTubeVideoDownload', async function () {
+      // videoQualityInfos.shift()
 
-		const url = await vscode.window.showInputBox()
-			.then((data) => data.trim())
-		if (url==null) return
-		const infos = await (await ytdl.getBasicInfo(url))
-		const title=infos.videoDetails.title
-		const videoQualityInfos = infos.formats.filter((info) => {
-			return info.mimeType.includes('video/mp4')
-		}).map((data) => {
-			return {
-				label: `${title} - ${data.qualityLabel}`,
-				detail: `${data.averageBitrate>4e+6?`${data.averageBitrate/(8e+6)} MB`:`${data.averageBitrate/(8000)} KB`}`,
-				itag:data.itag.toString()
-			}
-		})
+      const option = await vscode.window.showQuickPick(videoQualityInfos, {
+        matchOnDetail: true,
+        canPickMany: false,
+      })
 
-		videoQualityInfos.shift()
-		const option = await vscode.window.showQuickPick(videoQualityInfos, {
-			matchOnDetail: true, canPickMany: false
-		})
+      if (option == null) return
+      const path = await vscode.window.showOpenDialog({
+        canSelectFolders: true,
+        canSelectMany: false,
+        openLabel: "Save here",
+        canSelectFiles: false,
+        defaultUri: vscode.Uri.parse(title),
+      })
+      // console.log(path);
+      if (path == null) return
+      const downloadPath = `${path[0].fsPath}/${date.getTime()}.mp4`
+      const videoFormat = await ytdl.chooseFormat(infos.formats, {
+        quality: option.itag,
+      })
+      // console.log(videoFormat);
 
-		if (option==null) return
-		const path=await vscode.window.showOpenDialog({
-			canSelectFolders:true,
-			canSelectMany:false,
-			openLabel:'Save here',
-			canSelectFiles:false,
-		})
-		const createPath=`${path[0].fsPath}/${option.label}.mp4`
-		fs.writeFileSync(createPath,"dhhdhd")
-		// const async  = await vscode.window.showInputBox().then(function(text){
-		// 	return text
-		// })
-		// const url=await url
-		// ytdl('https://www.youtube.com/watch?v=MRum4_A7nB0')
-		// .pipe(fs.createWriteStream('D:\\download/video.mp4'))
+      ytdl(url, { format: videoFormat }).pipe(
+        fs.createWriteStream(downloadPath)
+      )
 
-		 vscode.window.showInformationMessage('Save here')
-	});
+      vscode.window.showInformationMessage("Downloading..")
+      // vscode.window.withProgress({
+      // 	location: vscode.ProgressLocation.Notification,
+      // 	title:'Downloading',
+      // 	cancellable:true
+      // },(progress,token)=>{
+      // 	token.onCancellationRequested(()=>{
+      // 		console.log("Canceled");
+      // 	})
+      // 	return p
+      // })
+    }
+  )
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
 
-function deactivate() { }
+function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
+  activate,
+  deactivate,
 }
